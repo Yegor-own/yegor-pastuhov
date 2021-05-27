d30befcfb8cc4ed4a5e7c0e9bc94d954983494f6
Yegor-own <egor891712@gmail.com>
2021-05-27 18:50:20 +0500

    add parallax switcher

--- js/index.js
+++ js/index.js
@@ -1,44 +1,62 @@
 'use strict'
 
 window.onload = function () {
-	const parallax = document.querySelector('body')
-
-	if (parallax) {
-		const content = document.querySelector('.parallax__container')
-		const space = document.querySelector('.images-parallax__space')
-		const stars = document.querySelector('.images-parallax__stars')
-
-		const forSpace = 40
-		const forStars = 20
-
-		const speed = 0.1
-
-		let posX = 0, posY = 0
-		let coordXproc = 0, coordYproc = 0
-
-		function setMouseParallaxStyle() {
-			const distX = coordXproc - posX
-			const distY = coordYproc - posY
-
-			posX += (distX * speed)
-			posY += (distY * speed)
-
-			space.style.cssText = `transform: translate(${posX / forSpace}%, ${posY / forSpace}%);)`
-			stars.style.cssText = `transform: translate(${posX / forStars}%, ${posY / forStars}%);)`
-
-			requestAnimationFrame(setMouseParallaxStyle)
+	function parallax(par) {
+		const parallax = par
+		
+		if (parallax) {
+			const content = document.querySelector('.parallax__container')
+			const space = document.querySelector('.images-parallax__space')
+			const stars = document.querySelector('.images-parallax__stars')
+			
+			const forSpace = 40
+			const forStars = 20
+			
+			const speed = 0.1
+			
+			let posX = 0, posY = 0
+			let coordXproc = 0, coordYproc = 0
+			
+			function setMouseParallaxStyle() {
+				const distX = coordXproc - posX
+				const distY = coordYproc - posY
+				
+				posX += (distX * speed)
+				posY += (distY * speed)
+				
+				space.style.cssText = `transform: translate(${posX / forSpace}%, ${posY / forSpace}%);)`
+				stars.style.cssText = `transform: translate(${posX / forStars}%, ${posY / forStars}%);)`
+				
+				requestAnimationFrame(setMouseParallaxStyle)
+			}
+			setMouseParallaxStyle()
+			
+			parallax.addEventListener("mousemove", function (e) {
+				if (!on) return false;
+				const parallaxWidth = parallax.offsetWidth
+				const parallaxHeight = parallax.offsetHeight
+	
+				const coordX = e.pageX - parallaxWidth / 2
+				const coordY = e.pageY - parallaxHeight / 2
+	
+				coordXproc = coordX / parallaxWidth * 100
+				coordYproc = coordY / parallaxHeight * 100
+			})
 		}
-		setMouseParallaxStyle()
-
-		parallax.addEventListener("mousemove", function (e) {
-			const parallaxWidth = parallax.offsetWidth
-			const parallaxHeight = parallax.offsetHeight
-
-			const coordX = e.pageX - parallaxWidth / 2
-			const coordY = e.pageY - parallaxHeight / 2
-
-			coordXproc = coordX / parallaxWidth * 100
-			coordYproc = coordY / parallaxHeight * 100
-		})
 	}
+	let on = true;
+	switcher.onclick = function() {
+		if (!on) {
+			on = true;
+			parallax(document.querySelector('body'));
+			console.log('on');
+		}
+		else {
+			on = false;
+			parallax(false);
+			console.log('off');
+		}
+	}
+
+	parallax(document.querySelector('body'));
 }
